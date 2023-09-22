25841329d7b5bfe494834ec41205a4e861f960d1
5aWTF <133536968+5aWTF@users.noreply.github.com>
2023-09-22 21:45:56 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -3,10 +3,16 @@
 # Stop the processes using stop.sh (if running)
 ./stop.sh
 
-# Start the main script (mpd2hls.sh) with the same arguments
+# Start the main script (mpd2hls_v2.sh) with the same arguments
 if [ $# -eq 4 ]; then
-    ./mpd2hls.sh -folder "$1" -file "$2" -url "$3" -key "$4" &
+    ./mpd2hls_v2.sh -folder "$1" -file "$2" -url "$3" -key "$4"  &
 else
-    echo "Usage: $0 nameOfTheFolder nameOfFile 'your_url_here' 'your_key_here'"
+    echo "Usage: $0 -folder StreamFolder_Name -file NameOfFile_WITHOUT_EXTENSIONS -url 'Stream_URL' -key 'your_key_and_kid_here' &"
+    echo "-------"
+    echo "-custom-proxy | For custom Proxy Server"
+    echo "-use-shaka | For Shaka Packager, Shaka Packager must be installed"
+    echo "-log-no-print | Log output of ffmpeg and N_m3u8D-RE and hide print while running."
+    echo "-no-logs-or-print | Does not log ffmpeg and N_m3u8D-RE and hide as well prints while running."
+    echo "-------"
     exit 1
 fi
